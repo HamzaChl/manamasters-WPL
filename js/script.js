7388fe51c4e7386c912f837cd01b379f6c6d3ba9
Charaf Inaoui <charaf.inaoui@outlook.com>
2024-03-01 23:50:05 +0000

    update laoding page js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const projectHolders = document.querySelectorAll(
+      ".project-holder:not(.accepted)"
+    );
+  
+    projectHolders.forEach(function (projectHolder) {
+      projectHolder.addEventListener("click", function () {
+        document.getElementById("popup").style.visibility = "visible";
+        document.getElementById("popup").style.opacity = "1";
+      });
+    });
+  
+    document.getElementById("close").addEventListener("click", function () {
+      document.getElementById("popup").style.visibility = "hidden";
+      document.getElementById("popup").style.opacity = "0";
+    });
+  });
