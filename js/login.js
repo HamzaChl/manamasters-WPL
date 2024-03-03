f125e780b403b491886d797ffc170d861cd6d6c2
Imrxng <148759783+Imrxng@users.noreply.github.com>
2024-03-03 17:50:16 +0100

    login js aangepast om preventdefault te kunnen doen

--- js/login.js
+++ js/login.js
@@ -1,10 +1,15 @@
 let registerButton = document.getElementById("Registreer");
-
+const loginButton = document.getElementById("loginButton");
+let login = true;
 registerButton.addEventListener("click", function (e) {
     e.preventDefault();
-    const loginButton = document.getElementById("loginButton");
     const h1Login = document.getElementById("h1Login");
     loginButton.value = "Registreer";
     h1Login.textContent = "Registreer"
     registerButton.style.visibility = "hidden";
+    login = false;
+});
+
+loginButton..addEventListener("click", function (e) {
+    e.preventDefault();
 });
