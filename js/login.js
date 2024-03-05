f4099effdaa97cb86f0759ead2699be2ec0bcc69
Ghaddoura Imran (s148286) <imran.ghaddoura@student.ap.be>
2024-03-05 17:54:18 +0100

    Probleem met js gefixt

--- js/login.js
+++ js/login.js
@@ -1,12 +1,19 @@
 let registerButton = document.getElementById("registerButton");
 const loginButton = document.getElementById("loginButton");
 let login = true;
+
 registerButton.addEventListener("click", function (e) {
     e.preventDefault();
     const h1Login = document.getElementById("h1Login");
-    loginButton.value = "Registreer";
-    h1Login.textContent = "Registreer"
-    registerButton.textContent = "login";
+    if (loginButton.value === "Login") {
+        loginButton.value = "Registreer";
+        h1Login.textContent = "Registreer"
+        registerButton.textContent = "login";
+    } else {
+        loginButton.value = "Login";
+        h1Login.textContent = "Login"
+        registerButton.textContent = "Registreer";
+    }
     login = false;
 });
 
